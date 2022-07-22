9d44a5fdf3f1971f740b0e26e13ba1bbe51d345d
panha-gms <hu-panha@global-mobility-service.com>
2022-07-22 16:45:14 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,8 @@
 const { defineConfig } = require("cypress");
-import _ from 'loadash'
-import del from 'del'
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('after:spec', (spec, results) => {
-        if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = _.some(results.tests, (test) => {
-            return _.some(test.attempts, { state: 'failed' })
-          })
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            return del(results.video)
-          }
-        }
-      })
+      
     },
   },
 });
