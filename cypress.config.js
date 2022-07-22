4a050de584e43c34b9d8362a6c15b68405fa4679
panha-gms <hu-panha@global-mobility-service.com>
2022-07-22 16:16:24 +0700

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  videoCompression: 15,
+  videoCompression: false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = _.some(results.tests, (test) => {
+            return _.some(test.attempts, { state: 'failed' })
+          })
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            return del(results.video)
+          }
+        }
+      })
+    }
+  }
+  
 });
